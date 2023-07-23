0216d517dbbf1c0361b3d3f1c47498f74f7bc4f7
dan-mm <33732895+dan-mm@users.noreply.github.com>
2023-07-23 17:05:44 +0200

    Update build-containers.sh

--- docker/auxiliary-containers/build-containers.sh
+++ docker/auxiliary-containers/build-containers.sh
@@ -3,10 +3,11 @@
 set -e
 
 # Get the list of subdirectories within "auxiliary-containers" directory containing a Dockerfile
-subdirs=($(find ./docker/auxiliary-containers -type f -name 'Dockerfile' -exec dirname {} \;))
+a
 
 # Loop through each subdirectory, build and push the Docker image
 for subdir in "${subdirs[@]}"; do
+  subdir=$(basename "${subdir}")
   docker buildx build \
     --push \
     --tag "ghcr.io/${GITHUB_REPOSITORY}/${subdir}:latest" \
