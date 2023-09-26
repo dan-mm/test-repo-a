527c4723f93d099b931eb16f5d9d1b127bbc6e43
dan-mm <33732895+dan-mm@users.noreply.github.com>
2023-09-26 15:36:13 +0200

    Update build-containers.sh

--- docker/auxiliary-containers/build-containers.sh
+++ docker/auxiliary-containers/build-containers.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
-set -e
+# the first arguement should be the version number
+version=$1
+
+# check that version is a non-empty string representing a number
+if ! [[ ${version} =~ ^[0-9]+$ ]]; then
+  echo "Usage: $0 <version-number>"
+  echo "Example: $0 1"
+  exit 1
+fi
+
+# The names of the folders within "auxiliary-containers" must match the repository name in dockerhub!
 
 # Get the list of subdirectories within "auxiliary-containers" directory containing a Dockerfile
 subdirs=($(find ./docker/auxiliary-containers -type f -name 'Dockerfile' -exec dirname {} \;))
@@ -10,7 +21,7 @@ for subdir in "${subdirs[@]}"; do
   folder=$(basename "${subdir}")
   docker buildx build \
     --push \
-    --tag "greencoding/${folder}:latest" \
+    --tag "greencoding/${folder}:v1.${version}" \
     --platform linux/amd64,linux/arm64 \
     "${subdir}"
 done
